a840c4bbca6ff66ec2299f5be721dbe5c5337c7d
getandpost <551718487@qq.com>
2019-01-29 22:38:41 +0800

    独立用户系统

--- backend/models/LoginForm.php
+++ backend/models/LoginForm.php
@@ -1,8 +1,9 @@
 <?php
-namespace common\models;
+namespace backend\models;
 
 use Yii;
 use yii\base\Model;
+use common\models\Admin;
 
 /**
  * Login form
@@ -70,7 +71,7 @@ class LoginForm extends Model
     protected function getUser()
     {
         if ($this->_user === null) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = Admin::findByUsername($this->username);
         }
 
         return $this->_user;
